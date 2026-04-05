75aff56db4984e1be86140b012bc2e5094bdf806
BayramovNicat <bayramov.nicat@hotmail.com>
2026-04-06 03:39:25 +0400

    Bundle frontend dependencies locally

---
+++ scripts/bundle.js
@@ -0,0 +1,10 @@
+// Bundle HTMX and Alpine.js
+import htmx from 'htmx.org'
+import Alpine from 'alpinejs'
+
+// Make them globally available
+window.htmx = htmx
+window.Alpine = Alpine
+
+// Start Alpine
+Alpine.start()
