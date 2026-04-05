49681eaee46586a1f5614254adef6a26b5be679c
BayramovNicat <bayramov.nicat@hotmail.com>
2026-04-06 03:05:20 +0400

    Replace Tailwind CDN with production build setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./ui/**/*.html",
+    "./ui/**/*.go",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
